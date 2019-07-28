77b703c2437981a4f08178b6eafcbf2391f8bacf
PraiseSunAsh <690163223@qq.com>
2019-07-28 17:17:33 +0800

    append

--- mpvueLearning/src/pages/list/list.vue
+++ mpvueLearning/src/pages/list/list.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>
-    list组件    
+  <div class>
+    <swiper indicator-dots="true" indicator-active-color="green" indicator-color="pink">
+      <swiper-item class item-id>
+        <image src="/static/images/gjjs.png" />
+      </swiper-item>
+      <swiper-item class item-id>
+        <image src="/static/images/gjjs.png" />
+      </swiper-item>
+      <swiper-item class item-id>
+        <image src="/static/images/gjjs.png" />
+      </swiper-item>
+      <swiper-item class item-id>
+        <image src="/static/images/gjjs.png" />
+      </swiper-item>
+    </swiper>
+    <div>
+      <ListTmp/>
+    </div>
   </div>
 </template>
 <script>
+//导入组件
+import ListTmp from "../list_template/list_template.vue";
 export default {
-  
-}
+  //注册当前组件
+  components: { ListTmp }
+};
 </script>
 <style>
-
+.listContainer swiper {
+  width: 100%;
+  height: 400rpx;
+}
+.listContainer swiper image {
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ mpvueLearning/src/pages/list_template/list_template.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <img src="/static/images/bysj.png" alt="">
+      <span>2077</span>
+    </div>
+    <p>哈哈哈</p>
+    <img src="/static/images/cate1.png " alt="">
+    <p>这里是一段话</p>
+    <div>
+      <img src="/static/images/gyzdh.jpeg" alt="">
+      <span>66</span>
+      <img src="/static/images/more.png" alt="">
+      <span>88</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
